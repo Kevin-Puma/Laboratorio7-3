a732cddf4da8301560627825dd42ca5e5ce0d53e
User <User@DESKTOP-CRJD420>
2023-10-04 19:28:38 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio7-3/Program.cs
@@ -0,0 +1,17 @@
+﻿int añoInicio = 1900;
+int añoFin = 2023;
+
+Console.WriteLine("Años bisiestos desde 1900 hasta 2023");
+
+for (int año = añoInicio; año <= añoFin; año++)
+{
+    if (EsAñoBisiesto(año))
+    {
+        Console.WriteLine(año);
+    }
+}
+
+static bool EsAñoBisiesto(int año)
+{
+    return (año % 4 == 0 && año % 100 != 0) || (año % 400 == 0);
+}
